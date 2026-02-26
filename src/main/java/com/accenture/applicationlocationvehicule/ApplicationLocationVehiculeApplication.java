bd83992b979f39c14563c7dfa68e29162d78762a
gabriel-S-rab <serguei.roos@laposte.net>
2026-02-26 11:45:04 +0100

    first commit

---
+++ src/main/java/com/accenture/applicationlocationvehicule/ApplicationLocationVehiculeApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.applicationlocationvehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLocationVehiculeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLocationVehiculeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accenture/applicationlocationvehicule/ApplicationLocationVehiculeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.applicationlocationvehicule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLocationVehiculeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
